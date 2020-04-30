70c7e6ddeb60f549af70b16f9f905e215bbe0e62
Janusz <janklyz@gmail.com>
2020-04-30 18:22:40 +0200

    adasdasd

---
+++ test5/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  test5
+//
+//  Created by Janusz  on 30/04/2020.
+//  Copyright © 2020 Janusz . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    print("dupaaa")
+    }
+
+
+}
+
